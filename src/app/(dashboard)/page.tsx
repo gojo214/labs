bbfc65fc22c67bf733cb9e0b91eb8e3b799cb9fb
gojo214 <gojojujustu4@gmail.com>
2026-03-12 00:01:03 +0530

    01:setup

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardView } from '@/features/dashboard/views/dashboard-view'
+import React from 'react'
+
+const Page = () => {
+  return (
+   <DashboardView/>
+  )
+}
+
+export default Page
